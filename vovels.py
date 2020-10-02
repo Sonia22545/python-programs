e54c68fc060794bed6685ade08b4b61d0e89fa50
Robin22545 <72149623+Robin22545@users.noreply.github.com>
2020-10-02 19:41:56 +0530

    Create vovels.py
    This program checks whether the entered letter is a vowel or not .

---
+++ vovels.py
@@ -0,0 +1,7 @@
+
+let = (input("Enter a letter :"))
+list =["a","e","i","o","u"]
+if let.lower() in list[0:5]:
+    print ("The letter is a vowel !")
+else :
+    print("the letter is not a vowel ")
